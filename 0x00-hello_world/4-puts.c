0aa01e05987ff4fd9a850c1c78aa3a3109aff5cf
TebogoMailula <tebzangwana486@gmail.com>
2023-06-15 14:30:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program with puts function
+ * Return : 0(succesful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
