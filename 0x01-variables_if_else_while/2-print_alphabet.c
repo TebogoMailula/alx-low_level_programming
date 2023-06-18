e432cee00a17f76e0e7ee58a53c3f24ffb84bb46
TebogoMailula <tebzangwana486@gmail.com>
2023-06-18 11:14:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
