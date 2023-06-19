4bd5b6a774b8109edc29b3f7c6868dd013e6dab1
TebogoMailula <tebzangwana486@gmail.com>
2023-06-18 18:27:15 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0; i < 9; i++)
+for (i = '0'; i < '9'; i++)
 {
-for (j = i; j <= 9; j++)
+for (j = i; j <= '9'; j++)
 {
 if (i != j)
 {
 putchar(i);
 putchar(j);
-if (i == 8 && j == 9)
+if (i == '8' && j == '9')
 {
 continue;
 }
