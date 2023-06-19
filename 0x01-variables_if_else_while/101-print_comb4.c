416110b40f37a4249e17a6f3a9679ee200ad81aa
TebogoMailula <tebzangwana486@gmail.com>
2023-06-18 19:13:03 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - prints all possible different combinations of three digits
+ * main - print all possible different combinations of three digits
  * Return: 0 (Success)
  */
 int main(void)
@@ -15,15 +15,14 @@ for (i = '0'; i < '9'; i++)
 for (j = i + 1; j <= '9'; j++)
 {
 for (k = j + 1; j <= '9'; k++)
+{
 if ((j != i) != k)
 {
 putchar(i);
 putchar(j);
 putchar(k);
 if (i == '7' && j == '8')
-{
 continue;
-}
 putchar(',');
 putchar(' ');
 }
