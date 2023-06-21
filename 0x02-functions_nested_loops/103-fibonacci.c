d0b7bc5b07385fc9c6f83298cfa1c62b4191c636
TebogoMailula <tebzangwana486@gmail.com>
2023-06-21 15:46:17 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -  program that finds and prints the sum of the even-valued terms
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+unsigned long int j, k, next, sum;
+j = 1;
+k = 2;
+sum = 0;
+for (i = 0; i <= 33; i++)
+{
+if (j < 100000 && (j % 2) == 0)
+{
+sum = sum + j;
+}
+next = j + k;
+j = k;
+k = next;
+}
+printf("%lu", sum);
+printf("\n");
+return (0);
+}
