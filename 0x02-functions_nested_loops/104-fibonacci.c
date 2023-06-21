c79aa7f6cfea3c4dae4e48d155a3f43045c46e3e
TebogoMailula <tebzangwana486@gmail.com>
2023-06-21 16:39:52 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,7 +31,7 @@ for (i = 92; i < 99; i++)
 {
 printf("%lu", aft1 + (aft2 / l));
 printf("%lu", aft2 % l);
-aftl = aft1 + bef1;
+aft1 = aft1 + bef1;
 bef1 = aft1 - bef1;
 aft2 = aft2 + bef2;
 bef2 = aft2 - bef2;
