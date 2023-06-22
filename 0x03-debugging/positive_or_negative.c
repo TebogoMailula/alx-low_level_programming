4d231986a1a3e69137a41aaec8640da6d789b027
TebogoMailula <tebzangwana486@gmail.com>
2023-06-22 07:17:11 -0700

    0x03-debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - test for positive or negative
+ * Return: 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("%d is %s\n", i, "negative");
+}
+else if (i > 0)
+{
+printf("%d is %s\n", i, "positive");
+else
+{
+printf("%d is %s\n", i, "zero");
+}
+return (0);
+}
